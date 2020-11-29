2dbefa2c471ec9334326456f014a0a542f965812
Anthony Potts <anthonypotts@Anthonys-MacBook-Pro-2.local>
2020-11-29 14:39:38 -0600

    add html

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -132,16 +144,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-});
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+    const pageHTML = generatePage(portfolioData);
 
-// const pageHTML = generatePage(name, github);
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
+  });
